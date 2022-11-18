a3b614e64f87384bade8145791a2758602433925
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-18 15:46:15 +0100

    Added the first informations

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,18 @@
 /* ----------------------------------------------------------------
-INIZIALIZZAZIONE E DICHIARAZIONE */
+INIZIALIZZAZIONE E DICHIARAZIONE DI VARIABILI E COSTANTI */
+
+// * INIZIALIZZAZIONE
+
+// ? PRELIEVO DAL DOM
+const nameInputArea = document.getElementById('name');
+const surnameInputArea = document.getElementById('surname');
+const favouriteColorInputArea = document.getElementById('favourite-color');
+
+
+
+// * DICHIARAZIONE 
+// ? 
+
 
 
 
@@ -7,3 +20,8 @@ INIZIALIZZAZIONE E DICHIARAZIONE */
 
 /* ----------------------------------------------------------------
 MAIN CODE */
+alert('Benvenuto!');
+alert('Per andare avanti dovrai inserire alcuni dati che sono: il nome , il cognome e il colore preferito');
+
+
+const name = prompt('Q')
